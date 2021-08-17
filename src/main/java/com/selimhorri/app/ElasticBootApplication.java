ae81b32e57c33a4e3f6dbca6beb3b2ce062a6e06
SelimHorri <horri.slim@gmail.com>
2021-08-18 00:19:48 +0100

    init

---
+++ src/main/java/com/selimhorri/app/ElasticBootApplication.java
@@ -0,0 +1,13 @@
+package com.selimhorri.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticBootApplication.class, args);
+	}
+
+}
